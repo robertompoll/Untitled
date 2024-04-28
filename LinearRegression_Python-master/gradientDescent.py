595d2795857f8225bfda830ea1ba0862d83e13b0
robertompoll <168246956+robertompoll@users.noreply.github.com>
2024-04-28 20:50:59 -0300

    gradientDescent.py

--- LinearRegression_Python-master/gradientDescent.py
+++ LinearRegression_Python-master/gradientDescent.py
@@ -1,14 +1,6 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Dec 31 13:43:35 2018
 
-GRADIENTDESCENT Performs gradient descent to learn theta
-theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by 
-taking num_iters gradient steps with learning rate alpha
-
-@author: gabi
-"""
+GRADIENTDESCENT Processa o gradiente para aprender theta
+theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) atualiza o valor de theta ao usar num_iters com a razão de aprendizado alpha
 
 def gradientDescent(x, y, theta, alpha, num_iters):
     
@@ -16,46 +8,30 @@ def gradientDescent(x, y, theta, alpha, num_iters):
     import pandas as pd
     import computeCost as cc
     
-    # Lenght of y
     m = len(y)
     
-    # List witn the number of iterations
     iterations = list(range(num_iters))
     
-    # Number of features
-    num_of_feat = len(x.columns)
-    
-    # Gradient descent term initialization ((h(x)-y)*x)
     term = pd.DataFrame(np.zeros([m,num_of_feat]))
     
-    # Jhist variable creation
     Jhist = pd.DataFrame(iterations)
     
-    # thetahist variable creation
     thetahist = pd.DataFrame(np.zeros([num_iters,num_of_feat]))
     
-    # Gradiante descent for iterations 
     for i in range(num_iters):
 
-        # The general term for the gradient descent (hip*theta)-y
         gen_term = np.dot(x,theta)-y
         
-        # Theta0 and theta 1 calculation
         for d in range(num_of_feat):
             
-            # Element Wise multiplication (gen_term*theta1)
             term.loc[:,d] = gen_term.mul(x.iloc[:,d],axis=0)
         
-            # Compute the gradient descent
             theta.iloc[d,:] =  theta.iloc[d,:] - ((alpha/m) * np.sum(term.iloc[:,d]))
         
-            # Compute the theta history
             thetahist.iloc[i,d] = float(theta.iloc[d,:])
         
-        # Cost function History
         Jhist.iloc[i,0] = float(cc.computeCost(x,y,theta))
         
-    # Mean and Standard head index
     theta.columns = ["Theta"]
     Jhist.columns = ["Jhist"]
     
