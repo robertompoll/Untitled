0f64bfe18bc4660d9cef11846a71a1438aacf23b
robertompoll <168246956+robertompoll@users.noreply.github.com>
2024-04-28 20:56:39 -0300

    Update computeCost.py

--- LinearRegression_Python-master/computeCost.py
+++ LinearRegression_Python-master/computeCost.py
@@ -1,17 +1,1 @@
-def computeCost(x, y, theta):
-    
-    import numpy as np
-    
-    m = len(y)
-    
-    J = 0
-    
-    hip = np.dot(x,theta) 
-    
-    squaErr = np.power((hip - y),2)
-    
-    J =  (1 / (2 * m)) * (np.sum(squaErr))
-    
-    J.columns = ["J"]
-    
-    return J
+
